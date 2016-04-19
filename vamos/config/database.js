fa48c8d2c41b2b135c04c68e6d6c015b99f4bcd9
unknown <sapirmonza@gmail.com>
2016-04-19 13:56:46 +0300

    commit
    test2

---
+++ vamos/config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+// looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+module.exports = {
+  'url': 'mongodb://sapirmonza:sos1988!@ds011261.mlab.com:11261/vamos'
+};
