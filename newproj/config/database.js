01f241351685d9ece01dc4dd8c28f53029f3dab1
unknown <sapirmonza@gmail.com>
2016-04-19 17:24:54 +0300

    Remove duplicated directory

--- newproj/config/database.js
+++
@@ -1,6 +0,0 @@
-// config/database.js
-// looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-
-module.exports = {
-  'url': 'mongodb://sapirmonza:sos1988!@ds011261.mlab.com:11261/vamos'
-};
